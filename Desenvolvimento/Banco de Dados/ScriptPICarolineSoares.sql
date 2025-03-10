f8977a231f40efe687d76f241a697a9997af3eab
adevcaroline <caroline.soares@sptech.school>
2025-03-10 14:41:03 -0300

    Atualizando o meu Banco de Dados

--- Desenvolvimento/Banco de Dados/ScriptPICarolineSoares.sql
+++ Desenvolvimento/Banco de Dados/ScriptPICarolineSoares.sql
@@ -23,7 +23,7 @@ idMedicao int primary key auto_increment,
 dataHora datetime,
 concentracaoGases INT NOT NULL,
 	CONSTRAINT ckTaxa 
-		CHECK (taxaConcentracao in (15,30,50)), 
+		CHECK (concentracaoGases in (15,30,50)), 
 statusAlerta VARCHAR(30)
 	CONSTRAINT ckstatus
 		CHECK (statusAlerta in ('Normal', 'Atenção', 'Crítico'))
@@ -38,25 +38,38 @@ numero INT NOT NULL,
 cnpj CHAR(14) UNIQUE NOT NULL
 );
 
-INSERT INTO cadastro (nome, senha, email) VALUES
-('Ana Karoline', 'aninha2000', 'ana.barrocal@sptech.com'),
-('Camila Jodoi', 'camilinha2000', 'camila.jodai@sptech.com'),
-('Caroline Soares','carolzinha2000', 'caroline.soares@sptech.com'),
-('Guilherme Mendes', 'guilherminho200', 'guilherme.mendes@sptech.com'),
-('João Lucas Reis', 'joaozinho2000', 'joao.lcarvalho@sptech.com'),
-('Letícia Rodrigues', 'leticinha2000', 'leticia.rodrigues@sptech.com'),
-('Rayssa Casagrande', 'rayssinha2000', 'rayssa.casagrande@sptech.com');
+INSERT INTO usuario (senha, cnpj) VALUES
+('aninho2000', '12345678000190'),
+('camilinho2000', '98765432000112'),
+('carolzinho2000', '56789123000134'),
+('guilherminha200', '43210987000156'),
+( 'joana2000', '11222333000178'),
+('leticinho2000', '77888999000100'),
+('rayssinho2000', '34567890000123');
 
-INSERT INTO sensor (nome, tipo, taxaConcentracao, statusSensor) VALUES
-('MQ-2', 'Sensor gás', '15', 'Ativo'),
-('MQ-3', 'Sensor gás', '30', 'Inativo'),
-('MQ-4', 'Sensor gás', '50', 'Ativo');
+INSERT INTO sensor (nome, tipo, statusSensor) VALUES
+('MQ-2', 'Sensor gás', 'Ativo'),
+('MQ-3', 'Sensor gás', 'Inativo'),
+('MQ-4', 'Sensor gás', 'Ativo');
 
 INSERT INTO condominio (nome, logradouro, cep, numero, cnpj) VALUES
 ('Osasco Residencial Life','O Trabuco Jornal', '02723456', 247, '12345678000190'),
 ('Osasco Residencial Death','O Vale da Morte', '66666666', 666, '12345678000191'),
 ('Osasco Residencial Sky','O Vale da Vida', '77777777', 777, '12345678000192');
 
-SELECT nome, cnpj FROM condominio WHERE idCondominio = 2;
-SELECT nome, tipo,  statusSensor FROM sensor WHERE idSensor = 2; 
-SELECT nome, email FROM cadastro WHERE idCadastro = 2; 
+INSERT INTO medicao (dataHora, concentracaoGases, statusAlerta) VALUES
+('2025-03-10 08:30:00', 15, 'Normal'),
+('2025-03-10 09:00:00', 30, 'Atenção'),
+('2025-03-10 09:30:00', 50, 'Crítico'),
+('2025-03-10 10:00:00', 30, 'Normal');
+
+
+SELECT * FROM condominio WHERE logradouro LIKE '%l';
+SELECT * FROM sensor WHERE statusSensor NOT LIKE 'Inativo';
+SELECT * FROM usuario WHERE idUsuario = 2;
+
+SELECT dataHora AS 'Data e Hora',
+concentracaoGases AS 'Concentração de Gases',
+statusAlerta AS 'Status'
+FROM medicao;
+
